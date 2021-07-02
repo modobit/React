2883a161d5d884fec5ae539a1c483ccd6e1cc756
modo <modobit@users.noreply.github.com>
2021-07-02 13:54:56 -0400

    Create Products.js

---
+++ myapp/src/Products.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'; 
+import axios from 'axios';
+import useFetch from 'react-fetch-hook';
+
+function Product(){
+
+    const url = `https://60df3c6fabbdd9001722d337.mockapi.io/api/v1/products/1`;
+    const[product, setProduct] = useState(null)
+
+    let content = null
+
+   useEffect(() => {
+        axios.get(url)
+    .then(response => {
+        setProduct(response.data)
+    }) 
+} , [url])
+
+if(product) {
+     content = 
+     <div>
+         <h1>{product.name}</h1>
+         <img src={product.avatar} />
+        <img src={product.image} />
+        <p>{product.description}</p>
+
+     </div>
+    
+}
+
+    return (
+        <div>
+            {content}
+        </div>
+    )
+    
+}
+
+
+export default Product;
